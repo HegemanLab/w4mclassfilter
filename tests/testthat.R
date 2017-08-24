581104357ee7424be53818e82b62f66e08eaa7e6
Art Eschenlauer <eschen@alumni.princeton.edu>
2017-08-24 13:53:22 -0500

    eliminated unicode charaters

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ requireNamespace("testthat")
 library("w4mclassfilter")
 
 # The following line causes a warning in devtools::check() -
-#   WARNING '::' or ':::' import not declared from: ‘testthat’
+#   WARNING '::' or ':::' import not declared from: 'testthat'
 # but I have found no way to suppress it without importing testthat into the package.
 # The problem is that 'testthat' is not needed by end users of the package.
 testthat::test_check("w4mclassfilter")
